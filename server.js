fd111ba6be6bac4ee1d46eac65583ad3e5897d29
Greg Hawthorne <ghawthorne@me.com>
2019-10-06 18:04:57 -0700

    <uploading to heroku>

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
